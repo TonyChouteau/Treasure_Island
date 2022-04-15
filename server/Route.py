f72a1baa1a1651ecc695189031887b62e0223e24
tony.7@hotmail.fr <tony.7@hotmail.fr>
2022-04-16 01:27:40 +0200

    comment websocket implementation

--- server/Route.py
+++ server/Route.py
@@ -1,14 +1,17 @@
 class Route:
 
     def __init__(self):
+        # Define all routes
         self.route = {
             "hello": self.hello,
             "test": self.test
         }
 
+    # Check the availability of the route
     def exists(self, event_type):
         return True if self.route.get(event_type) else False
 
+    # Handle route access
     def handle(self, event_type, event):
         if self.exists(event_type):
             self.route.get(event_type)(event)
--- server/WebSocket.py
+++ server/WebSocket.py
@@ -9,25 +9,31 @@ from server.Route import Route
 class WebSocket:
 
     def __init__(self):
+        # Handle Routes
         self.route = Route()
-
+        # Run Websocket
         asyncio.run(self.main())
 
+    # Each client has an handler
     async def handler(self, websocket):
         while True:
             try:
+                # Wait for a new message from the client
                 message = await websocket.recv()
             except websockets.ConnectionClosedOK:
+                # If client is disconnected
                 break
 
+            # Get content from message
             event = json.loads(message)
 
             event_type = event.get("type")
             if event_type is None:
                 break
 
+            # If message type exists and can be handled, handle it
             self.route.handle(event_type, event)
 
     async def main(self):
         async with websockets.serve(self.handler, "", 8001):
-            await asyncio.Future()  # run forever
+            await asyncio.Future()  # run forever, wait for new connection
