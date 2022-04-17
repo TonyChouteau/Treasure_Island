40ad772896186fd588e809a54dc30f4691e9c7d5
tony.7@hotmail.fr <tony.7@hotmail.fr>
2022-04-18 00:31:31 +0200

    simplify

--- test/compass_test.py
+++ test/compass_test.py
@@ -1,40 +1,39 @@
 import math
 
 compass = {0: "W", 1: "SW", 2: "S", 3: "SE", 4: "E", 5: "NE", 6: "N", 7: "NW"}
-px = int(input("Player X : "))
-py = int(input("Player Y : "))
+px = 0
+py = 0
 x = int(input("Point X : "))
 y = int(input("Point Y : "))
 
-direction = int(input("Direction : "))
+# direction = int(input("Direction : "))
 
 
-if direction in range(8):
-    print(compass[direction])
 
-    # Center of the circle is the (px,py) point, atan2(py-y,px-x) gives the radian angle
-    # to the chosen point within that circle
-    angle = math.degrees(math.atan2(y - py, x - px))
-    print("\nAngle :", angle,"°")
+# Center of the circle is the (px,py) point, atan2(py-y,px-x) gives the radian angle
+# to the chosen point within that circle
+angle = math.degrees(math.atan2(y - py, x - px))
+print("\nAngle :", angle,"°")
 
-    # Offset to get 8 directions with diagonal frontiers
-    offset = (angle + 202.5) / 45   
-    if int(offset) == offset:
-        print("EDGE CASE") # Right on the frontiers
-    else:
-        # Reduce angle to [0,7] interval, called region
-        region = math.floor(offset) % 8
-        print(region)
+# Offset to get 8 directions with diagonal frontiers
+offset = (angle + 202.5) / 45
+if int(offset) == offset:
+    print("EDGE CASE") # Right on the frontiers
+else:
+    # Reduce angle to [0,7] interval, called region
+    region = math.floor(offset) % 8
+    # print(region)
 
-        # If the point is in the chosen direction, region and direction are now equal
-        if direction == region:
-            print(
-                "Point is", compass[direction], "compared to the selected player (center)"
-            )
-        else:
-            print(
-                "Point is not",
-                compass[direction],
-                "compared to the selected player (center) but rather",
-                compass[region],
-            )
+    # If the point is in the chosen direction, region and direction are now equal
+    print(compass[region])
+    # if direction == region:
+    #     print(
+    #         "Point is", compass[direction], "compared to the selected player (center)"
+    #     )
+    # else:
+    #     print(
+    #         "Point is not",
+    #         compass[direction],
+    #         "compared to the selected player (center) but rather",
+    #         compass[region],
+    #     )
