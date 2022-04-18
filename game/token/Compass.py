9850812f29916abd448776fea045414b665ec760
Thomas Lepercq <tlepercq@enssat.fr>
2022-04-18 17:13:24 +0200

    HintDistrict basis and code formatting

--- game/token/Compass.py
+++ game/token/Compass.py
@@ -1,15 +1,19 @@
 from game.Position import Position
 from math import degrees, atan2, floor
 
-class Compass:
 
+class Compass:
     def __init__(self) -> None:
         pass
 
     def get_direction(self, player: Position, treasure: Position) -> int:
         angle = degrees(atan2(treasure.y - player.y, treasure.x - player.x))
-        offset = (angle + 202.5) / 45   # Offset to get positive angles and 8 directions with diagonal frontiers
-        return floor(offset) % 8        # Reduce angle to [0,7] interval, giving the direction
+        offset = (
+            angle + 202.5
+        ) / 45  # Offset to get positive angles and 8 directions with diagonal frontiers
+        return floor(offset) % 8  # Reduce angle to [0,7] interval, giving the direction
 
-    def test_position(self, player: Position, treasure: Position, direction: int) -> bool:
-        return self.get_direction(player,treasure) == direction
+    def test_position(
+        self, player: Position, treasure: Position, direction: int
+    ) -> bool:
+        return self.get_direction(player, treasure) == direction
--- game/token/HintDistrict.py
+++ game/token/HintDistrict.py
@@ -1,4 +1,13 @@
-class HintDistrict:
+from game.Position import Position
 
-    def __init__(self) -> None:
-        pass
+
+class HintDistrict:
+    def __init__(self, id: int) -> None:
+        self.id = id
+
+    def get_district(self, point: Position) -> int:
+        # return map(point.x, point.y)["district"]
+        return -1
+
+    def is_valid(self, treasure: Position) -> bool:
+        return self.id != self.get_district(treasure)
