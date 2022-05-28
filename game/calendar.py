5e7d14313ac3a3a8366a4f321d3c9aa51f72b7d1
Thomas Lepercq <tlepercq@enssat.fr>
2022-05-28 22:27:21 +0200

    snake_case instead of camelCase and comments

--- game/calendar.py
+++ game/calendar.py
@@ -5,27 +5,31 @@ class Calendar:
     def __init__(self, players):
         self.players = players
         self.date = 0
-        self.startingBlocks = sorted(self.players, key=lambda x: x.turnNumber)
-        self.calendarBoard = []
-        self.events = ""
-        if len(self.players) % 2 == 0:
-            self.calendarBoard = [0 for _ in range(19)]
+        self.starting_blocks = sorted(
+            self.players, key=lambda x: x.turnNumber
+        )  # Virtual spots before the board for the players, placed by turn order
+        self.calendar_board = []  # Board spots (calendar) for players to move in
+        self.events = ""  # Chain of event codes in order with the calendar
+
+        if len(self.players) % 2 == 0:  # 2 or 4 players
+            self.calendar_board = [0 for _ in range(19)]
             self.events = "1213104502050505000"
         else:
-            self.calendarBoard = [0 for _ in range(17)]
+            self.calendar_board = [0 for _ in range(17)]
             self.events = "12130145250550000"
 
     def next(self):
-        if len(self.startingBlocks) != 0:
-            self.calendarBoard[self.date] = self.startingBlocks.pop()
+        # Move the players around
+        if len(self.starting_blocks) != 0:
+            self.calendar_board[self.date] = self.starting_blocks.pop()
         else:
-            self.calendarBoard[self.date] = self.calendarBoard[
+            self.calendar_board[self.date] = self.calendar_board[
                 self.date - len(self.players)
             ]
-            self.calendarBoard[self.date - len(self.players)] = 0
-            
+            self.calendar_board[self.date - len(self.players)] = 0
+
         self.date += 1
-        if self.date > len(self.calendarBoard):
+        if self.date > len(self.calendar_board):
             pass  # end
         if self.events[self.date] != 0:
             pass  # Handle various events
